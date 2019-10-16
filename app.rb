58bcf91f585b36497d1d3fac80d843e91b08c755
fthiounn <fthiounn@gmail.com>
2019-10-16 13:31:17 +0200

    update, boardcase and bord almost implemented

--- app.rb
+++ app.rb
@@ -8,4 +8,28 @@ require_relative "lib/game"
 require_relative "lib/application"
 
 
-Game.new.perform
+#Game.new.perform
+
+		puts "╔═════════╗" 
+		puts "║    "+"■".red+"    ║" 
+		puts "║  "+"■".red+"   "+"■".red+"  ║" 
+		puts "║    "+"■".red+"    ║" 
+		puts "╚═════════╝"
+
+
+		puts "╔═════════╗" 
+		puts "║  "+"■".light_blue+"   "+"■".light_blue+"  ║" 
+		puts "║    "+"■".light_blue+"    ║" 
+		puts "║  "+"■".light_blue+"   "+"■".light_blue+"  ║" 
+		puts "╚═════════╝"
+
+
+
+		print "╔═════════╗".green
+		print "╔═════════╗".green
+		puts "╔═════════╗".green
+
+		5.times do |n|
+			puts n
+			
+		end
